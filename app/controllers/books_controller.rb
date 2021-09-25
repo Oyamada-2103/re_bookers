6ee7254b11d91ea9618062ba3822d942df4bf6e4
Oyamada-2103 <ta9.syukatu@gmail.com>
2021-09-25 12:54:15 +0000

    controller.top

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,7 +13,7 @@ class BooksController < ApplicationController
     # エラーメッセージを出すときのrender部分で使う
 
     if @book.save
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), notice:'Book was successfully created.'
     else
       render :index
     end
@@ -28,15 +28,19 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @books = Book.all
+    @book = Book.find(params[:id])
+      if @book.update(book_params)
+        redirect_to book_path(@book.id), notice:'Book was successfully updated.'
+      else
+        render "edit"
+      end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
+    redirect_to books_path , notice:'Book was successfully destroyed.'
   end
 
   private
